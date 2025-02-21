34d5e3e16ae20681e04ae7759d55123345227b9f
krishnaa-1 <krishnamalviya272@gmail.com>
2025-02-21 20:04:13 +0530

    contact done

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <h1>Contact</h1>
+        <Link to="/"> <button>Redirect me to Home Page</button> </Link>
+        </div>
   )
 }
 
