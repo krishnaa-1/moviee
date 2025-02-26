3fe87fc550dc87484246b3e0d9027ec890c2f596
krishnaa-1 <krishnamalviya272@gmail.com>
2025-02-26 15:50:36 +0530

    cooment add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from './About';
 import Contact from './Contact';
 import HomePage from "./HomePage";
 import {Routes, Route} from 'react-router-dom';
+import MoviePageDetails from './MoviePageDetails';
 
 function App() {
   return (
@@ -9,10 +10,12 @@ function App() {
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/moviedetails/:name/:description/:genre/:image" element={<MoviePageDetails />}></Route>
       </Routes>
   );
 }
 //done
+//kashihs ka kaam
 
 
 export default App;
